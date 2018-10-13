fd049dea924c4bc254572e0df73336eff52f1df8
Robin Cura <robin.cura@gmail.com>
2018-10-13 16:52:44 +0200

    add shiny slides

---
+++ src/reactivite3.R
@@ -0,0 +1,55 @@
+# Global
+library(shiny)
+library(tidyverse)
+
+# UI
+ui <- fluidPage(
+    fluidRow(
+        column(4, sliderInput(inputId = "input_a", label = "Nb Observations",
+                              min = 1, max = 150, value = 150)),
+        column(4, sliderInput(inputId = "input_b", label = "Nb barres",
+                              min = 2, max = 30, value = 20)),
+        column(4, selectInput(inputId = "input_c", label = "Couleur",
+                              choices = c("Grey", "Red", "Blue", "Green")))
+    ),
+    fluidRow(
+        column(6, dataTableOutput(outputId = "output_d")),
+        column(6, plotOutput(outputId = "output_e"))
+    )
+)
+
+# Server
+server <- function(input, output, session) {
+    table_filtree <- reactive({
+        iris %>%
+            sample_n(input$input_a) %>%
+            arrange(Species, Sepal.Length)
+    }) 
+    output$output_d <- renderDataTable({
+        table_filtree()
+    })
+    output$output_e <- renderPlot({
+        ggplot(table_filtree()) +
+            geom_histogram(aes(x = Sepal.Length),
+                           bins = input$input_b,
+                           fill = input$input_c)
+    })
+    
+    observe({
+        if (input$input_a > 30){
+            updateSliderInput(inputId = "input_b",
+                              session = session,
+                              min = 2,
+                              max = 30)
+        } else {
+            updateSliderInput(inputId = "input_b",
+                              session = session,
+                              min = 2,
+                              max = input$input_a,
+                              value = input$input_a/2)
+        }
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
